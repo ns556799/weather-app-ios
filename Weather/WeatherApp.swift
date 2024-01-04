d1e747ef581694360f6051299131d6e26a7f4822
Nivethanan Suresh <nivsuresh06@gmail.com>
2024-01-04 05:11:25 +0000

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Nivethanan Suresh on 04/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
